fa8296351d65883a2555fa2eef25c388d835d89f
jacob knudsen <jhakeknudsen@protonmail.com>
2022-03-17 16:05:31 -0600

    rollbar attampt 3

--- server/server.js
+++ server/server.js
@@ -9,9 +9,8 @@ var rollbar = new Rollbar({
   captureUnhandledRejections: true,
 })
 
-
 // record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+rollbar.log('Hello world!') 
 
 const app = express()
 app.get('/', (req, res) =>{

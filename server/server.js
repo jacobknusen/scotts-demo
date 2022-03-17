f0894fe0f0022040b398fe159b8318cbffe17102
jacob knudsen <jhakeknudsen@protonmail.com>
2022-03-17 15:56:48 -0600

    rollbar

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const path = require('path')
 
 // include and initialize the rollbar library with your access token
-const Rollbar = require('rollbar')
-const rollbar = new Rollbar({
-  accessToken: 'fc85b379582247b49cc157e02fe46180',
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'f36a898d6c454be791044b8793623295',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
+
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')

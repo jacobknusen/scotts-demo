f4219938dac7b0b2dc189a83c9ee0886e47d2029
jacob knudsen <jhakeknudsen@protonmail.com>
2022-03-17 23:11:11 -0600

    add further functionlity

--- server/server.js
+++ server/server.js
@@ -25,19 +25,29 @@ let students = []
 app.get('/api/students', (req, res) =>{
     res.status(200).send(students)
 })
+const index = students.findIndex(studentName=> studentName === name)
 
 app.post('/api/students', (req, res) =>{
     let {name} = req.body
     name = name.trim()
 
-    students.push(name)
-    
-   
-    rollbar.log('student was added succesfully', {author: 'jake', type: 'manual'})
+    const index = students.findIndex(studentName=> studentName === name)
 
-    res.status(200).send(students)
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('student was added succesfully', {author: 'jake', type: 'manual'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('no name given')
+    } else{
+        rollbar.critical('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
 })
 
+
+//pre built code from rollbar we import right here. using dot.use
 app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
